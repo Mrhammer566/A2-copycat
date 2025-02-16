03b73c00a2208fc505faa23ff4c77afc341850fc
Jim Utton <jimmyutton@gmail.com>
2025-02-16 15:15:36 -0500

    Credits

---
+++ Assets/Acknowledgements/Thank yous.cs
@@ -0,0 +1,7 @@
+/// Rock band
+///Publishers: Harmonix Music Systems, Mad Catz, MTV Games, Electronic Arts, Warner Bros. Games, EA Mobile
+///Developers: Harmonix Music Systems, Backbone Entertainment, MORE
+///Designers: Harmonix Music Systems, Alli Tresher, Dan Teasdale
+/// WTF Wars - Rhythm Edition - A musical game for GDevelop
+/// ulisesfreitas
+/// https://ulisesfreitas.itch.io/wtf-wars-rhythm-edition-a-musical-game-for-gdevelop
